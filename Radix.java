2fc10c2f4a6c53a919fd16536f7b5321fdfd75ac
Samson Badlia <sbadlia00@stuy.edu>
2019-04-02 11:27:02 -0400

    Able to find how many passes needed

--- Radix.java
+++ Radix.java
@@ -14,4 +14,26 @@ public class Radix{
     return num % 10;
   }
 
+  public static int longest(int[] data){
+    int max = Math.abs(data[0]); //starting reference
+    for (int i = 1; i <= data.length - 1; i++){
+      if (Math.abs(data[i]) >= max) max = Math.abs(data[i]); //if value at is greater, new largest
+    }
+    int digits = 0; //tracks number of digits for longest number
+    while (max > 0){
+      max /= 10;
+      digits++;
+    }
+    return digits;
+  }
+
+  public static void main(String[] args) {
+    int[] a = new int[]{1,2,-31};
+    int[] b = new int[]{-11111, 10293,3, 46};
+    int[] c = new int[]{ -121111 , 10293 , 3 , 46};
+    System.out.println("Should be: 2 \nActual is:" + longest(a));
+    System.out.println("Should be: 5 \nActual is:" + longest(b));
+    System.out.println("Should be: 6 \nActual is:" + longest(c));
+
+  }
 }
